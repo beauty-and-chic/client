c54b0ea09a6c4e7c80f672a72159c584450f1d6f
Durin TASONDOCK <temgoua484@gmail.com>
2023-02-12 11:45:09 +0100

    designing logo

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import './App.css'
+
+import logo from './assets/logo.png'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <img src={logo} alt="christelle shopping logo" />
+      <div>
+        <h1>CHRISTELLE</h1>
+        <h2>SHOPPING</h2>
+      </div>
+    </div>
+  )
+}
+
+export default App
